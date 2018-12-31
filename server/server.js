f8d8b49cbd8e0ec80508c7011e163f6f88aab8f5
Sarah Pickrahn <spickrahn@gmail.com>
2018-12-31 14:22:34 -0800

    working app, created navbar, homepage and ttt board empty files;

--- server/server.js
+++ server/server.js
@@ -6,10 +6,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'../public')));
 
-// app.get('/', (req, res) => {
-//   res.send('hiiiiiii from express!!');
-// });
-
 const PORT = 1337;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}........o.o`);
