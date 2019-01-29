dde7d63ec170f7892912714ff74b2aa2d785c7c0
Sarah Pickrahn <spickrahn@gmail.com>
2019-01-29 00:47:04 -0800

    begin converting db functions to promises instead of callbacks

--- database/db.js
+++ database/db.js
@@ -121,29 +121,53 @@ function move(gameId, gameInfo, index, callback) {
   client.query(moveQuery, values, callback);
 }
 
+//================================= AUTH FUNCTIONS ==============================================
+
+// function getUserByEmail(email, callback) {
+//   let query = 'SELECT id FROM users WHERE email = $1';
+//   let values = [email];
+//   client.query(query, values, callback);
+// }
+
+// function getUserByUsername(username, callback) {
+//   let query = 'SELECT id FROM users WHERE username = $1';
+//   let values = [username];
+//   client.query(query, values, callback);
+// }
+//ADD IN UNIQUE CONSTRAINT TO DB
+
 function userInfoIsValid(email, username, password, password2) {
-  let validEmail = email.trim() !== '' ? true : false;
-  let validUsername = username.trim() !== '' ? true : false;
-  let validPassword = (password.length > 5 && password.length < 73 && password === password2) ? true : false;
-  return validEmail && validUsername && validPassword;
+  if (email.trim() === "") {
+    return Promise.reject(new Error("Invalid email."));
+  }
+  if (username.trim() === "") {
+    return Promise.reject(new Error("Invalid username."));
+  }
+  if (password.length < 6 || password.length > 72) {
+    return Promise.reject(new Error("Password must be between 6 and 72 characters."));
+  }
+  if (password !== password2) {
+    return Promise.reject(new Error("Passwords must match."));
+  }
+  return Promise.resolve();
 }
 
 function createUserAccount(email, username, password, password2, callback) {
   //verify username, email and password
+  //check if email is in use
+  //check if username is in use
   if (userInfoIsValid(email, username, password, password2)) {
     //use bcrypt to hash password
-    bcrypt.hash(password, 10, function(err, hash) {
-      if (err) {
-        console.log(err);
-      } else {
-        let query = 'INSERT INTO users (username, password, email, created) VALUES ($1, $2, $3, CURRENT_DATE) RETURNING id, username, email, created';
-        let values = [username, hash, email];
-        client.query(query, values, callback);
-      }
+    return bcrypt.hash(password, 10).then(function(hash) {
+      let query = 'INSERT INTO users (username, password, email, created) VALUES ($1, $2, $3, CURRENT_DATE) RETURNING id, username, email, created';
+      let values = [username, hash, email];
+      client.query(query, values);
     });
   } else {
     //return error for invalid email, username or password
-    console.log('Invalid email, username or password.');
+    console.log();
+    return Promise.reject(new Error('Invalid email, username or password.'))
+    //return failed promise
     //let errorMessage = 'Please make sure your password is between 6 and 72 characters, and that you have entered your email and username!';
   }
 
